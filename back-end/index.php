3f0017b1b2f2cc9534f677e960cf7e672d411b20
ManuelF <manuel.floriani1999@gmail.com>
2023-06-22 18:06:37 +0200

    Creazione file json

--- back-end/index.php
+++ back-end/index.php
@@ -4,18 +4,8 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 header('Content-Type: application/json'); // Funzione che specifica il formato in  cui sono scritti i dati
 
-$tasks = [
-    [
-        'name' => 'Spippolare nei menu di Obsidian',
-        'state' => 'completed'
-    ],
-    [
-        'name' => 'Coding Challenge',
-        'state' => 'todo'
-    ],
-];
-
-echo json_encode($tasks); // Restituisce la stringa del contenuto dell'array sotto forma di json
+$toDoStr = file_get_contents('data.json');
+echo $toDoStr;
 
 
 ?>
