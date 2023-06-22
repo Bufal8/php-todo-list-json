b1eaf2c55d5807076d39872bf26c5824b6b69401
ManuelF <manuel.floriani1999@gmail.com>
2023-06-22 15:42:50 +0200

    Primo push

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+header('Content-Type: application/json'); // Funzione che specifica il formato in  cui sono scritti i dati
+
+$tasks = [
+    [
+        'name' => 'Spippolare nei menu di Obsidian',
+        'state' => 'completed'
+    ],
+    [
+        'name' => 'Coding Challenge',
+        'state' => 'todo'
+    ],
+];
+?>
